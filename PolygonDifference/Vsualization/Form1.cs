f6a851b4a95648c40e13138453f7fe4653d33fea
RomanDubinin <Roman230394@yandex.ru>
2015-05-23 00:39:12 +0500

    пустое окно

---
+++ PolygonDifference/Vsualization/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Vsualization
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+			var graphics = Graphics.FromImage(pictureBox1.Image);
+
+		}
+	}
+}
